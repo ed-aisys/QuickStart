003c3193183bd39cf3890668f666ab66c1b3b7a2
Chivier Humber <chivier.humber@outlook.com>
2024-05-16 15:12:14 +0100

    fix: add +x chmod for docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -9,4 +9,5 @@ sudo apt-get update
 sudo apt-get install docker-compose-plugin
 
 curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
